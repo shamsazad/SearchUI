e8dbf328f27acadbb781809da81a29d43a27b811
badaldesai <b_desai@encs.concordia.ca>
2016-09-13 21:37:18 -0400

    teest

---
+++ src/js/components/Layout.js
@@ -0,0 +1,14 @@
+/**
+ * Created by mac on 9/12/16.
+ */
+import React from "react";
+
+export default class Layout extends React.Component {
+    render() {
+        return (
+           <form>
+                <input type="text" placeholder = "Search City" />
+            </form>
+        );
+    }
+}
