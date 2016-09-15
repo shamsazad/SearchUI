373fd56ee1f58648851da06b86ea17433308012f
Badal <badaldesai@gmail.com>
2016-09-15 16:49:11 -0400

    Seperate Search Component from Layout

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -2,13 +2,12 @@
  * Created by mac on 9/12/16.
  */
 import React from "react";
+import SearchBar from "./SearchBar";
 
 export default class Layout extends React.Component {
     render() {
         return (
-           <form>
-                <input type="text" placeholder = "Search City" />
-            </form>
+           <SearchBar />
         );
     }
 }
