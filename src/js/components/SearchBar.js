17ed2cb4b00e2c3037f641ffcbe77cb8760cd504
Badal <badaldesai@gmail.com>
2016-09-16 16:38:48 -0400

    Added axios to post data to server

--- src/js/components/SearchBar.js
+++ src/js/components/SearchBar.js
@@ -2,6 +2,7 @@
  * Created by bdesai on 2016-09-15.
  */
 import React from "react";
+import axios from "axios";
 
 export default class SearchBar extends React.Component {
     constructor(props) {
@@ -10,7 +11,13 @@ export default class SearchBar extends React.Component {
     }
     handleChange(e) {
         const city = e.target.value;
-        
+        axios.post(url, {
+            city:city,
+        }).then(function(response){
+            dispatch(addPost(response.body))
+        }).catch(function (response) {
+            request =response;
+        });
     }
     render() {
         return(
