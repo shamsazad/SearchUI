e11bd2d633a32f2129869b3cadd788b8874f9f3b
badaldesai <b_desai@encs.concordia.ca>
2016-10-15 23:55:14 -0400

    Added one of the dependencies in package. Change on change function to press enter

--- src/js/components/SearchBar.js
+++ src/js/components/SearchBar.js
@@ -3,26 +3,29 @@
  */
 import React from "react";
 import axios from "axios";
+const url = 'http://localhost:8081/yuul/search/';
 
 export default class SearchBar extends React.Component {
     constructor(props) {
         super(props);
         this.handleChange.bind(this);
     }
-    handleChange(e) {
-        const city = e.target.value;
-        axios.post(url, {
-            city:city,
-        }).then(function(response){
-            dispatch(addPost(response.body))
-        }).catch(function (response) {
-            request =response;
-        });
+    handleChange (e) {
+        if (e.key === 'Enter') {
+            const city = e.target.value;
+            axios.post(url, {
+                location: city,
+            }).then(function (response) {
+                dispatch(addPost(response.body))
+            }).catch(function (response) {
+                request = response;
+            });
+        }
     }
     render() {
         return(
             <form>
-                <input type = "text" placeholder = "Search City" onChange={this.handleChange} />
+                <input type = "text" placeholder = "Search City" onKeyPress={this.handleChange} />
             </form>
         );
     }
