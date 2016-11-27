df59e7f08e8469d9cb684c057e191d4a56a26ade
badaldesai <b_desai@encs.concordia.ca>
2016-11-27 16:43:18 -0500

    Added date field

--- src/js/components/SearchBar.js
+++ src/js/components/SearchBar.js
@@ -3,8 +3,11 @@
  */
 import React from "react";
 import axios from "axios";
+import DatePicker from "react-datepicker";
 
 const url = 'http://localhost:8081/yuul/search/';
+
+require('react-datepicker/dist/react-datepicker.css');
 
 export default class SearchBar extends React.Component {
     constructor(props) {
@@ -32,11 +35,22 @@ export default class SearchBar extends React.Component {
     }
     render() {
         return(
-            <form>
-                <input type = "text" name = "location" placeholder = "Search City" required onChange={this.handleChange} className="search-bar" />
-                <input type = "date" name = "startDate" placeholder = "Start Date" onChange={this.handleChange}/>
-                <input type = "submit" value="Search" onClick={this.handleSubmit}/>
+            <div className="container">
+            <form className="form-inline">
+                <div className="col-lg-8">
+                    <div className="input-group">
+                    <input type = "text" className ="form-control" name = "location" placeholder = "Search City" required onChange={this.handleChange} />
+                    <span className="input-group-addon">
+                         <DatePicker name="startDate"
+                                     selected={this.state.startDate}
+                                     onChange={this.handleChange} />
+                    </span>
+                        <input type = "submit" value="Search" onClick={this.handleSubmit}/>
+                    </div>
+                </div>
+
             </form>
+            </div>
         );
     }
 }
