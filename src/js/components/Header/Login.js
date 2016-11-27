63b6a4dd6fab682cbd2278088d55c72fa7886f21
badaldesai <b_desai@encs.concordia.ca>
2016-11-26 23:52:07 -0500

    Removed remaining Radium for one of the component

--- src/js/components/Header/Login.js
+++ src/js/components/Header/Login.js
@@ -2,18 +2,7 @@
  * Created by bdesai on 2016-11-09.
  */
 import React from "react";
-import Radium from 'radium';
 
-const styles = {
-    base: {
-
-    },
-    each: {
-
-    }
-}
-
-@Radium
 export default class Login extends React.Component {
     render() {
         return (
