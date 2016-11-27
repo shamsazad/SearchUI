77840c562675e327d18fcb9348eaafd53b93ad03
badaldesai <b_desai@encs.concordia.ca>
2016-11-26 23:23:25 -0500

    Removed Radium for style purposes and added style.css

--- src/js/components/Header/Title.js
+++ src/js/components/Header/Title.js
@@ -2,19 +2,11 @@
  * Created by mac on 10/16/16.
  */
 import React from "react";
-import Radium from 'radium';
 
-const styles = {
-    base: {
-        textAlign: 'center'
-    }
-}
-
-@Radium
 export default class Title extends React.Component {
     render() {
         return (
-            <h1 style={styles.base}>Search Appartment</h1>
+            <h1 className="title">Search Appartment</h1>
         );
     }
 }
