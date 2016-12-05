2d35523ddea3192b335b07c7bd0468eddae82514
Badal Desai <badal.desai@hmhco.com>
2016-12-05 16:38:32 -0500

    Added some padding to make searchbar visible

--- src/js/components/SearchBar.js
+++ src/js/components/SearchBar.js
@@ -4,6 +4,7 @@
 import React from "react";
 import axios from "axios";
 import DatePicker from "react-datepicker";
+import moment from 'moment';
 
 const url = 'http://localhost:8081/yuul/search/';
 
@@ -14,15 +15,23 @@ export default class SearchBar extends React.Component {
         super(props);
         this.state = {
             location: "Montreal",
-            startDate: "",
+            startDate: moment(),
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleDateChange = this.handleDateChange.bind(this);
     }
 
     handleChange(e) {
         this.setState({[e.target.name]:e.target.value});
     }
+
+    handleDateChange(date) {
+        this.setState({
+            startDate: date
+        });
+    }
+
     handleSubmit () {
         console.log(this.state);
         axios.post(url,
@@ -36,20 +45,16 @@ export default class SearchBar extends React.Component {
     render() {
         return(
             <div className="container">
-            <form className="form-inline">
-                <div className="col-lg-8">
-                    <div className="input-group">
+                <form className="form-inline">
                     <input type = "text" className ="form-control" name = "location" placeholder = "Search City" required onChange={this.handleChange} />
                     <span className="input-group-addon">
-                         <DatePicker name="startDate"
+                         <DatePicker
                                      selected={this.state.startDate}
-                                     onChange={this.handleChange} />
+                                     onChange={this.handleDateChange} />
                     </span>
                         <input type = "submit" value="Search" onClick={this.handleSubmit}/>
-                    </div>
-                </div>
 
-            </form>
+                </form>
             </div>
         );
     }
