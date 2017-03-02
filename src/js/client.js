f3031ed75bb77e43f9703ddd0c5dd59e3de0dde8
Badal Desai <badal.desai@hmhco.com>
2017-03-02 14:48:22 -0500

    Added support for Postcss

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import Layout from "./components/Layout";
+require('../postcss/style.css');
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
