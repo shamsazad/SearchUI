a2c32c83a8761b5b1cb482889d410a1a47dd0381
Badal Desai <badal.desai@hmhco.com>
2017-09-18 17:13:30 -0400

    Working search bar

--- src/js/components/SearchBar.js
+++ src/js/components/SearchBar.js
@@ -6,25 +6,60 @@ import { searchCity } from '../actions/index';
 class SearchBar extends Component {
     constructor(props) {
         super(props);
-        this.state = { address: 'Montreal, QC' };
-        this.onChange = (address) => this.setState( { address })
+        this.handleSelect = this.handleSelect.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    handleFormSubmit = (event) => {
-        event.preventDefault();
+    handleSelect(address) {
+        geocodeByAddress(address)
+            .then((results) => getLatLng(results[0]))
+            .then(({lat, lng}) => {
+                console.log(this.props);
+                this.props.field.searchCity.lat = lat;
+                this.props.field.searchCity.lng = lng;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
-        geocodeByAddress(this.state.address)
-            .then(results => getLatLng(results[0]))
-            .then(latLng => console.log('Success', latLng))
-            .catch(error => console.log('Error', error))
+    handleChange(address) {
+        this.props.field.searchCity.address = address;
+        console.log(address);
     }
 
     render() {
+        const AutocompleteItem = ({ formattedSuggestion }) => (
+            <div className="Demo__suggestion-item">
+                <i className='fa fa-map-marker Demo__suggestion-icon'/>
+                <strong>{formattedSuggestion.mainText}</strong>{' '}
+                <small className="text-muted">{formattedSuggestion.secondaryText}</small>
+            </div>);
+
+        const inputProps = {
+            type: "text",
+            value: this.address,
+            onChange: this.handleChange,
+            autoFocus: true,
+            placeholder: "Search Places",
+            name: 'Demo__input',
+            id: "my-input-id",
+        }
+
         const { handleSubmit } = this.props;
+        console.log(this.props);
 
         return(
-            <form onSubmit={ handleFormSubmit }>
-                <Field name="searchCity" component={PlacesAutocomplete}/>
+            <form onSubmit={ handleSubmit }>
+                <Field
+                    name="searchCity"
+                    component={PlacesAutocomplete}
+                    type="search"
+                    onSelect={this.handleSelect}
+                    autocompleteItem={AutocompleteItem}
+                    onEnterKeyDown={this.handleSelect}
+                    inputProps={inputProps}
+                />
             </form>
         )
     }
