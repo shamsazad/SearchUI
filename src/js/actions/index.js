975052256d56317076681ed9a6380f4c5a416a9b
badaldesai <b_desai@encs.concordia.ca>
2017-10-10 00:33:14 -0400

    Remove coordinates logic

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
 
 export const SEARCH_CITY = 'SEARCH_CITY';
 
@@ -7,17 +6,10 @@ const URL = 'http://localhost:8081/';
 
 export function searchCity(props) {
 
-    return geocodeByAddress(props.placeSearch)
-        .then(results => {
-            return getLatLng(results[0]).then(result => {
-                const request = axios.post(`${URL}/`, {
-                    coordinates: result,
-                    props: props
-                });
-                return {
-                    type: SEARCH_CITY,
-                    payload: request
-                }
-            });
-        });
+    const request = axios.post(`${URL}/place`,props);
+
+    return {
+        type: SEARCH_CITY,
+        payload: request
+    }
 }
