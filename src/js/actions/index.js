36b10ce977963138f81330c9f42176a7636de421
badaldesai <b_desai@encs.concordia.ca>
2017-10-29 23:02:28 -0400

    Added action and reducer related to bounds change

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,15 +1,32 @@
 import axios from 'axios';
 
 export const SEARCH_CITY = 'SEARCH_CITY';
+export const CHANGE_BOUNDS = 'CHANGE_BOUNDS';
 
-const URL = 'http://localhost:8081/';
+const URL = 'http://localhost:8081/yuul/search';
 
 export function searchCity(props) {
-
-    const request = axios.post(`${URL}/address/`,props.placeSearch);
+    console.log(props);
+    const request = axios.get(`${URL}/${props.placeSearch}`);
 
     return {
         type: SEARCH_CITY,
         payload: request
     }
 }
+
+export function changeBounds(props) {
+    const request = axios.post(`${URL}/eventTrigger`, props);
+
+    return {
+        type: CHANGE_BOUNDS,
+        payload: request,
+        meta: {
+            debounce: {
+                time: 300,
+                key: CHANGE_BOUNDS
+            }
+        }
+    }
+}
+
--- src/js/reducers/reducer-properties.js
+++ src/js/reducers/reducer-properties.js
@@ -1,9 +1,11 @@
-import { SEARCH_CITY } from '../actions/index';
+import { SEARCH_CITY, CHANGE_BOUNDS } from '../actions/index';
 
-export default function (state = [], action) {
+export default function (state = {}, action) {
     switch (action.type) {
         case SEARCH_CITY:
-            return [ action.payload.data ];
+            return action.payload.data;
+        case CHANGE_BOUNDS:
+            return action.payload.data;
     }
     return state;
 }
